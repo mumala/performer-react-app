40ad88524d391e7f44941e71832e1f2308d63245
mumala <wnwoduf7@naver.com>
2023-05-29 00:58:16 +0900

    implement logout and resolve glitch issue

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
 import React, {useEffect, useState} from 'react';
-import {Container, List, Paper} from "@mui/material"
+import {AppBar, Button, Container, Grid, List, Paper, Toolbar, Typography} from "@mui/material"
 import AddTodo from './AddTodo';
-import {call} from "./service/ApiService";
+import {call, signout} from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     call("/todo", "GET", null)
-      .then((response) => setItems(response.data));
+      .then((response) => {
+        setItems(response.data)
+        setLoading(false);
+      });
   }, []);
 
   const addItem = (item) => {
@@ -39,15 +44,40 @@ function App() {
         </List>
       </Paper>
     );
-  
-  return (
+
+  let navigationBar = (
+    <AppBar position='static'>
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant='h6'>오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color='inherit' raised onClick={signout}>로그아웃</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  let todoListPage = (
     <div className='App'>
+      {navigationBar}
       <Container maxWidth="md">
         <AddTodo addItem={addItem}/>
         <div className='TodoList'>{todoItems}</div>
       </Container>
     </div>
   );
+
+  let loadingPage = <h1>로딩중...</h1>;
+  let content = loadingPage;
+
+  if(!loading){
+    content = todoListPage;
+  }
+
+  return <div className='App'>{content}</div>
 }
 
 export default App;
